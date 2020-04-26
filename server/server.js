bb4fa21370b2e002fcfa29d6dd9d908a1affcf13
mausinixterra <mausinixterra@gmail.com>
2020-04-26 12:43:11 -0500

    Restserver - usuario

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ require('./config/config');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
 const app = express();
 
 // parse application/x-www-form-urlencoded
@@ -11,40 +14,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
  
-app.get('/usuario', function (req, res) {
-  res.json('get usuario');
-});
-
-app.post('/usuario', function (req, res) {
-
-    let body = req.body;
-
-    if ( body.nombre === undefined ) {
-
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-  });
-
-app.put('/usuario/:id', function (req, res) {
-
-    let id =req.params.id;
-    res.json({
-        id
-    });
-
-  });
-
-app.delete('/usuario', function (req, res) {
-    res.json('delete usuario');
-  });
+app.use( require( './routes/usuario' ) );
  
+
+  mongoose.connect(process.env.URLDB, ( err, res ) => {
+    if ( err ) throw err;
+
+    console.log( 'Base de datos ONLINE ');
+  });
+
+
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto: ', process.env.PORT);
 });
